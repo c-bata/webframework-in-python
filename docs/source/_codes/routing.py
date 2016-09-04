e6e070faf40ec7b5578b486e92818503e5c02538
c-bata <contact@c-bata.link>
2016-09-04 09:53:04 +0900

    Update routing sample

--- docs/source/_codes/routing.py
+++ docs/source/_codes/routing.py
@@ -47,21 +47,22 @@ class App:
         return callback(env, start_response, **kwargs)
 
 
-# application
+# Application
 #############
 app = App()
 
 
-@app.route('^/$')
-def index(env, start_response):
-    start_response('200 OK', [('Content-type', 'text/plain; charset=utf-8')])
-    return [b'Top Page']
-
-
 @app.route('^/users/$')
-def index(env, start_response):
+def user_list(env, start_response):
     start_response('200 OK', [('Content-type', 'text/plain; charset=utf-8')])
     return [b'User List']
+
+
+@app.route('^/users/(?P<user_id>\d+)/$')
+def user_detail(env, start_response, user_id):
+    start_response('200 OK', [('Content-type', 'text/plain; charset=utf-8')])
+    res = 'Hello user {user_id}'.format(user_id=user_id)
+    return [res.encode('utf-8')]
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
