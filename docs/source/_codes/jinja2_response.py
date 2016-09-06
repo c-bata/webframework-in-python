5f95bad01083d204f7280c4602ba3fd866a1b720
c-bata <contact@c-bata.link>
2016-09-06 18:58:55 +0900

    Create jinja2_response exmaple

--- docs/source/_codes/jinja2_response.py
+++ docs/source/_codes/jinja2_response.py
@@ -1,3 +1,5 @@
+# Framework
+############
 import cgi
 import json
 from jinja2 import Environment, PackageLoader
@@ -156,3 +158,38 @@ class App:
 
     def __call__(self, env, start_response):
         return self.wsgi(env, start_response)
+
+
+# Application
+##############
+from app import App, Response, TemplateResponse, JSONResponse
+from collections import OrderedDict
+
+app = App(__name__)
+
+
+@app.route('^/$')
+def index(request):
+    return Response('Hello World')
+
+
+@app.route('^/users/$')
+def user_list(request):
+    title = 'ユーザ一覧'
+    users = ['user{}'.format(i) for i in range(10)]
+    response = TemplateResponse(filename='users.html', title=title, users=users)
+    return response
+
+
+@app.route('^/users/(?P<user_id>\d+)/$')
+def user_detail(request, user_id):
+    d = OrderedDict(
+        user=user_id,
+    )
+    response = JSONResponse(dic=d, indent=4)
+    return response
+
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+    httpd = make_server('', 8000, app)
+    httpd.serve_forever()
