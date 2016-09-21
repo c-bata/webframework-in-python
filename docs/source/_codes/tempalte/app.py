08651b9ccfb685fb86b620dcf81fc0935eb67ee3
c-bata <contact@c-bata.link>
2016-09-21 12:32:37 +0900

    Update docs templates

---
+++ docs/source/_codes/tempalte/app.py
@@ -0,0 +1,126 @@
+import os
+import re
+import cgi
+from urllib.parse import parse_qs
+from wsgiref.headers import Headers
+from jinja2 import Environment, FileSystemLoader
+
+
+def http404(request):
+    return Response('404 Not Found', status='404 Not Found')
+
+
+class Router:
+    def __init__(self):
+        self.routes = []
+
+    def add(self, method, path, callback):
+        self.routes.append({
+            'method': method,
+            'path': path,
+            'callback': callback
+        })
+
+    def match(self, method, path):
+        for r in filter(lambda x: x['method'] == method.upper(), self.routes):
+            matched = re.compile(r['path']).match(path)
+            if matched:
+                kwargs = matched.groupdict()
+                return r['callback'], kwargs
+        return http404, {}
+
+
+class Request:
+    def __init__(self, environ):
+        self.environ = environ
+        self._body = None
+
+    @property
+    def forms(self):
+        form = cgi.FieldStorage(
+            fp=self.environ['wsgi.input'],
+            environ=self.environ,
+            keep_blank_values=True,
+        )
+        params = {k: form[k].value for k in form}
+        return params
+
+    @property
+    def query(self):
+        return parse_qs(self.environ['QUERY_STRING'])
+
+    @property
+    def body(self) -> str:
+        if self._body is None:
+            content_length = int(self.environ.get('CONTENT_LENGTH', 0))
+            self._body = self.environ['wsgi.input'].read(content_length).decode('utf-8')
+        return self._body
+
+    @property
+    def text(self, charset='utf-8'):
+        return self.body.decode(charset)
+
+
+class Response:
+    default_status = '200 OK'
+    default_content_type = 'text/html; charset=UTF-8'
+
+    def __init__(self, body='', status=None, headers=None, charset='utf-8'):
+        self._body = body
+        self.status = status or self.default_status
+        self.headers = Headers()
+        self.charset = charset
+
+        if headers:
+            for name, value in headers.items():
+                self.headers.add_header(name, value)
+
+    @property
+    def body(self):
+        if isinstance(self._body, str):
+            return self._body.encode(self.charset)
+        return self._body
+
+    @property
+    def header_list(self):
+        if 'Content-Type' not in self.headers:
+            self.headers.add_header('Content-Type', self.default_content_type)
+        return self.headers.items()
+
+
+class TemplateResponse(Response):
+    default_content_type = 'text/html; charset=UTF-8'
+
+    def __init__(self, filename, status='200 OK', headers=None, charset='utf-8', **tpl_args):
+        self.filename = filename
+        self.tpl_args = tpl_args
+        super().__init__(body='', status=status, headers=headers, charset=charset)
+
+    def render_body(self, jinja2_environment):
+        template = jinja2_environment.get_template(self.filename)
+        return template.render(**self.tpl_args).encode(self.charset)
+
+
+class App:
+    def __init__(self, templates=None):
+        self.router = Router()
+        if templates is None:
+            templates = [os.path.join(os.path.abspath('.'), 'templates')]
+        self.jinja2_environment = Environment(loader=FileSystemLoader(templates))
+
+    def route(self, path=None, method='GET', callback=None):
+        def decorator(callback_func):
+            self.router.add(method, path, callback_func)
+            return callback_func
+        return decorator(callback) if callback else decorator
+
+    def __call__(self, env, start_response):
+        method = env['REQUEST_METHOD'].upper()
+        path = env['PATH_INFO'] or '/'
+        callback, kwargs = self.router.match(method, path)
+
+        response = callback(Request(env), **kwargs)
+        start_response(response.status, response.header_list)
+        if isinstance(response, TemplateResponse):
+            return [response.render_body(self.jinja2_environment)]
+        return [response.body]
---
+++ docs/source/_codes/tempalte/main.py
@@ -0,0 +1,31 @@
+from app import App, Response, TemplateResponse
+from wsgiref.simple_server import make_server
+
+
+app = App()
+
+
+@app.route('^/$', 'GET')
+def hello(request):
+    return Response('Hello World')
+
+
+@app.route('^/user/$', 'POST')
+def create_user(request):
+    return Response('User Created', status='201 Created')
+
+
+@app.route('^/user/$', 'GET')
+def users(request):
+    users = ['user%s' % i for i in range(10)]
+    return TemplateResponse('users.html', title='User List', users=users)
+
+
+@app.route('^/user/(?P<name>\w+)$', 'GET')
+def user_detail(request, name):
+    return Response('Hello {name}'.format(name=name))
+
+
+if __name__ == '__main__':
+    httpd = make_server('', 8000, app)
+    httpd.serve_forever()
