870ed5a1cf80ea30b3aad1a2f9fafbd29be83444
c-bata <contact@c-bata.link>
2016-09-21 12:50:45 +0900

    Add static middleware code

---
+++ docs/source/_codes/static_middleware/main.py
@@ -0,0 +1,35 @@
+from app import App, Response, TemplateResponse, JSONResponse
+import os
+from wsgiref.simple_server import make_server
+from wsgi_static_middleware import StaticMiddleware
+
+
+app = App()
+
+
+@app.route('^/$', 'GET')
+def hello(request):
+    return Response('Hello World')
+
+
+@app.route('^/user/$', 'POST')
+def create_user(request):
+    return JSONResponse({'message': 'User Created'}, status='201 Created')
+
+
+@app.route('^/user/$', 'GET')
+def users(request):
+    users = ['user%s' % i for i in range(10)]
+    return TemplateResponse('users.html', title='User List', users=users)
+
+
+@app.route('^/user/(?P<name>\w+)$', 'GET')
+def user_detail(request, name):
+    return Response('Hello {name}'.format(name=name))
+
+
+if __name__ == '__main__':
+    static_dirs = [os.path.join(os.path.abspath('.'), 'static')]
+    app = StaticMiddleware(app, static_dirs=static_dirs, static_root='static')
+    httpd = make_server('', 8000, app)
+    httpd.serve_forever()
